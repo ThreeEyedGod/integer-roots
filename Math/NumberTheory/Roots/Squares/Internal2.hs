bb2f22ccf5a90c9e5ef9980f04f7023fb017581d
Mande Burung <mandeburung@Mandes-MacBook-Air.local>
2025-07-02 12:55:43 +0530

    removed excess vector from tni sub-functions

--- Math/NumberTheory/Roots/Squares/Internal2.hs
+++ Math/NumberTheory/Roots/Squares/Internal2.hs
@@ -121,11 +121,11 @@ isqrtB n = fromInteger . theNextIterations . theFi . dgtsVecBase32__ . fromInteg
 -- | Iteration loop data - these records have vectors / lists in them
 data Itr = Itr {lv :: {-# UNPACK #-} !Int, vecW32_ :: {-# UNPACK #-} !(VU.Vector Word32), l_ :: {-# UNPACK #-} !Int#, yCumulative :: !Integer, iRem_ :: {-# UNPACK #-} !Integer, tb# :: {-# UNPACK #-} !FloatingX#} deriving (Eq)
 
-data LoopArgs = LoopArgs {position :: {-# UNPACK #-} !Int#, inArgs_ :: !IterArgs_, residuali32Vec :: !(VU.Vector Word32)} deriving (Eq)
+data LoopArgs = LoopArgs {position :: {-# UNPACK #-} !Int#, inArgs_ :: !IterArgs_} deriving (Eq)
 
 data ProcessedVec = ProcessedVec {firstTwo :: !(VU.Vector Word32), len :: !Int} deriving (Eq)
 
-data RestNextTwo = RestNextTwo {theRestVec :: !(VU.Vector Word32), firstWord32 :: {-# UNPACK #-} !Word32, secondWord32 :: {-# UNPACK #-} !Word32} deriving (Eq)
+data RestNextTwo = RestNextTwo {firstWord32 :: {-# UNPACK #-} !Word32, secondWord32 :: {-# UNPACK #-} !Word32} deriving (Eq)
 
 theFi :: VU.Vector Word32 -> Itr 
 theFi v 
@@ -145,15 +145,15 @@ theFi v
 
 {-# INLINE prepA_ #-}
 prepA_ :: Int# -> VU.Vector Word32 -> RestNextTwo
-prepA_ l# w32Vec = RestNextTwo w32Vec (VU.unsafeIndex w32Vec (I# l# - 2)) (VU.unsafeIndex w32Vec(I# l# - 1)) 
+prepA_ l# w32Vec = RestNextTwo (VU.unsafeIndex w32Vec (I# l# - 2)) (VU.unsafeIndex w32Vec(I# l# - 1)) 
 
 prepB_ :: Integer -> FloatingX# -> RestNextTwo -> IterArgs_
-prepB_ iRem tBFX# (RestNextTwo _ !n1_ !nl_) = IterArgs_ (intgrFrom3DigitsBase32 iRem (n1_, nl_)) (scaleByPower2 (intToInt64# 32#) tBFX#) -- sqrtF previous digits being scaled right here
+prepB_ iRem tBFX# (RestNextTwo !n1_ !nl_) = IterArgs_ (intgrFrom3DigitsBase32 iRem (n1_, nl_)) (scaleByPower2 (intToInt64# 32#) tBFX#) -- sqrtF previous digits being scaled right here
 {-# INLINE prepB_ #-}
 
 {-# INLINE prepArgs_ #-}
 prepArgs_ :: Itr -> LoopArgs
-prepArgs_ (Itr _ w32Vec l# _ iRem tBFX_#) = let !rnxt2@(RestNextTwo ri32Vec _ _) = prepA_ l# w32Vec in LoopArgs l# (prepB_ iRem tBFX_# rnxt2) ri32Vec
+prepArgs_ (Itr _ w32Vec l# _ iRem tBFX_#) = let !rnxt2@(RestNextTwo _ _) = prepA_ l# w32Vec in LoopArgs l# (prepB_ iRem tBFX_# rnxt2) 
 
 -- Keep it this way: Inlining this lowers performance.
 theNextIterations :: Itr -> Integer
@@ -163,9 +163,9 @@ theNextIterations itr@(Itr !currlen !w32Vec !l# !yCumulated !iRem !tbfx#) = tni 
       if I# l# == 0 || VU.null v
         then yC
         else
-          let (LoopArgs _ !inA_ !ri32V) = prepArgs_ (Itr cl v l# yC iR t#)
+          let (LoopArgs _ !inA_) = prepArgs_ (Itr cl v l# yC iR t#)
               (IterRes !yc !yTildeFinal !remFinal) = nxtDgtRem yC inA_ -- number crunching only
-           in tni (succ cl) ri32V (l# -# 2#) yc remFinal (fixTCFX# inA_ cl yTildeFinal) -- do not VU.force ri32V
+           in tni (succ cl) v (l# -# 2#) yc remFinal (fixTCFX# inA_ cl yTildeFinal) -- do not VU.force ri32V
 
 -- theNextIterations itr@(Itr currlen w32Vec l# yCumulated iRem tbfx#)
 --   | VU.null w32Vec = yCumulated
