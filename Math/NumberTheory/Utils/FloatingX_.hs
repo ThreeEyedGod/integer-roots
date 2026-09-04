8b6ab5be9e4ba134a32b6824fd64030a8f2dd57e
Mande Burung <mandeburung@Mandes-MacBook-Air.local>
2026-09-04 13:59:04 +0530

    some optoimizations

--- Math/NumberTheory/Utils/FloatingX_.hs
+++ Math/NumberTheory/Utils/FloatingX_.hs
@@ -235,7 +235,7 @@ unsafestDivFx# n@(FloatingX# !s1# !e1#) d@(FloatingX# !s2# !e2#) = FloatingX# (s
 {-# INLINE fsqraddFloatingX# #-}
 fsqraddFloatingX# :: FloatingX# -> FloatingX# -> FloatingX#
 fsqraddFloatingX# (FloatingX# !sA# 0#Int64) (FloatingX# !sC# 0#Int64) = FloatingX# (fmaddDouble# sA# sA# sC#) 0#Int64
--- fsqraddFloatingX# (FloatingX# !sA# !expA#) (FloatingX# !sC# ((==# 1# (eqInt64# expA#))-> True)) = FloatingX# (fmaddDouble# sA# sA# sC#) 0#Int64
+fsqraddFloatingX# (FloatingX# !sA# !expA#) (FloatingX# !sC# (\x -> isTrue#(x `eqInt64#` expA# ==# 1#) -> True)) = FloatingX# (fmaddDouble# sA# sA# sC#) expA#
 fsqraddFloatingX# (FloatingX# !sA# !expA#) (FloatingX# !sC# !expC#) = case upLiftDouble# sC# (int64ToInt# diff#) of sC_# -> FloatingX# (fmaddDouble# sA# sA# sC_#) twoTimesExpA# -- let !sC_# = updateDouble# sC# (int64ToInt# diff#) in FloatingX# (fmaddDouble# sA# sA# sC_#) twoTimesExpA#
   where
     !twoTimesExpA# = 2#Int64 `timesInt64#` expA#
